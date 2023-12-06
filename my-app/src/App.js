4bd3729b37cab19bfce4c9ac9ffc7877469f15d1
Gaurav Arora <gaurav.arora@turtlemint.com>
2023-12-06 18:06:25 +0530

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  useEffect(() => { // Correct usage of useEffect
-    console.log("🚀 ~ file: App.js:8 ~ useEffect ~ document.cookie:", document.cookie)
-    console.log(document.cookie); // This will log all cookies accessible to this domain
-  }, []);
+  
+  // useEffect(() => { // Correct usage of useEffect
+  //   console.log("🚀 ~ file: App.js:8 ~ useEffect ~ document.cookie:", document.cookie)
+  //   console.log(document.cookie); // This will log all cookies accessible to this domain
+  // }, []);
+
+  useEffect(() => {
+
+    window.addEventListener('message', function(event) {
+      console.log("Message received from the parent: " + event.data); // Message received from parent
+    });
+  },[])
 
   return (
     <h1>
